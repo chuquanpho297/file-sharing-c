33a2ef35d0a13d19bf806656780f0cbc563dad80
hung.lai <hung.lai@sotatek.com>
2024-12-28 22:38:27 +0700

    fix type

--- src/client.h
+++ src/client.h
@@ -1,3 +1,5 @@
+#include <json-c/json.h>
+
 #ifndef CLIENT_H
 #define CLIENT_H
 
@@ -16,8 +18,8 @@ void handle_folder_create_client(int sock, const char *folder_path, const char *
 void handle_folder_rename_client(int sock, const char *folder_path, const char *new_name, struct json_object *j);
 void handle_folder_copy_client(int sock, const char *from_folder, const char *to_folder, struct json_object *j);
 void handle_folder_move_client(int sock, const char *from_folder, const char *to_folder, struct json_object *j);
-void handle_folder_delete_client(int sock, const char *folder_path, struct json_object *j);
-void handle_folder_search_client(int sock, const char *search_term, struct json_object *j);
+void handle_folder_delete_client(int sock, const char *folder_path, struct json_object *jobj);
+void handle_folder_search_client(int sock, const char *search_term, struct json_object *jobj);
 void handle_folder_download_client(int sock, const char *folder_path, const char *folder_owner,
                                    const char *des_folder_path, struct json_object *j);
 void handle_folder_upload_client(int sock, const char *folder_path, const char *folder_name, struct json_object *j);
@@ -26,11 +28,11 @@ void handle_file_download_client(int sock, const char *file_path, const char *fi
 void handle_file_rename_client(int sock, const char *file_path, const char *new_name, struct json_object *j);
 void handle_file_copy_client(int sock, const char *file_path, const char *to_folder, struct json_object *j);
 void handle_file_move_client(int sock, const char *file_path, const char *to_folder, struct json_object *j);
-void handle_file_delete_client(int sock, const char *file_path, struct json_object *j);
+void handle_file_delete_client(int sock, const char *file_path, struct json_object *jobj);
 void handle_file_search_client(int sock, const char *file_name, struct json_object *jobj);
 void handle_set_folder_access(int sock, const char *folder_path, const char *access, struct json_object *jobj);
 void handle_logout(int sock, int *is_logged_in, char *current_user);
-void handle_file_search_client(int sock, const char *search_term, struct json_object *j);
+void handle_file_search_client(int sock, const char *search_term, struct json_object *jobj);
 void send_folder(int sock, const char *upload_folder_path, const char *des_folder_path);
 void handle_set_file_access(int sock, const char *file_path, const char *access, struct json_object *jobj);
 const char *get_filename(const char *path);
